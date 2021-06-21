a7da4cf32cd333fb195197ed274b20a3a908421e
junhyoung9702 <LEE@Leejunhyoung.(none)>
2021-06-21 21:39:10 +0900

    ARIMA order update

--- autoTrade/main.py
+++ autoTrade/main.py
@@ -1,5 +1,6 @@
 from get_coin_dayprice import get_coin_dayprice
-
+from generate_ARIMA_order import generate_ARIMA_order
 if __name__ == '__main__':
 
     get_coin_dayprice()
+    generate_ARIMA_order()
