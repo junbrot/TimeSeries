a53271587edd532786f7bae0c8779dc7ec257391
junhyoung9702 <LEE@Leejunhyoung.(none)>
2021-06-21 20:00:59 +0900

    backtest update

--- backtest/main.py
+++ backtest/main.py
@@ -1,8 +1,10 @@
 from get_dayprice import collect_dayprice_everyday
 from generate_training_test_data import generate_training_test_data
 from generate_ARIMA_model import generate_ARIMA_model
+from backtest import backtest
 if __name__ == '__main__':
 
-    # collect_dayprice_everyday()
-    # generate_training_test_data()
+    collect_dayprice_everyday()
+    generate_training_test_data()
     generate_ARIMA_model()
+    backtest()
